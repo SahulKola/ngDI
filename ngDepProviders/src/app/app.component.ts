ba805d3fcd5bcd772501ae7ce88b341a5f876b66
sahulkola <mail2sahul.js@gmail.com>
2024-03-26 13:02:07 +0530

    improve the useFactory provider example

--- ngDepProviders/src/app/app.component.ts
+++ ngDepProviders/src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injector } from '@angular/core';
 import { LoggerService } from './services/logger.service';
 import { ExpLoggerService } from './services/exp-logger.service';
-import { legacyLogger } from './services/logger.legacy';
-import { APP_CONFIG, AppConfig } from './services/config.token';
+import { APP_CONFIG } from './services/config.token';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +9,10 @@ import { APP_CONFIG, AppConfig } from './services/config.token';
   styleUrl: './app.component.scss',
   providers: [{
     provide: LoggerService,
-    useFactory: (appConfig: AppConfig) => {
-      return appConfig.experimentalEnabled ? new ExpLoggerService() : new LoggerService()
+    useFactory: (injector:Injector) => {
+      return injector.get(APP_CONFIG).experimentalEnabled ? new ExpLoggerService() : new LoggerService()
     },
-    deps: [APP_CONFIG]
+    deps: [Injector]
   }] 
 })
 export class AppComponent {
--- ngDepProviders/src/app/services/config.token.ts
+++ ngDepProviders/src/app/services/config.token.ts
@@ -7,7 +7,7 @@ export const APP_CONFIG = new InjectionToken<AppConfig>('APP_CONFIG', {
     providedIn: 'root',
     factory() {
         return {
-            experimentalEnabled: false
+            experimentalEnabled: true
         }
     },
 });
