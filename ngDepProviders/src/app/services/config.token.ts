86347b47ca67711ef222ed000dfa9a80897b5a39
sahulkola <mail2sahul.js@gmail.com>
2024-03-26 12:49:48 +0530

    add useValue provider example - with Injection Token

---
+++ ngDepProviders/src/app/services/config.token.ts
@@ -0,0 +1,13 @@
+import { InjectionToken } from "@angular/core";
+export interface AppConfig{
+    experimentalEnabled: boolean;
+}
+
+export const APP_CONFIG = new InjectionToken<AppConfig>('APP_CONFIG', {
+    providedIn: 'root',
+    factory() {
+        return {
+            experimentalEnabled: true
+        }
+    },
+});
--- ngDepProviders/src/app/services/exp-logger.service.ts
+++ ngDepProviders/src/app/services/exp-logger.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Logger } from '../logger';
+import { APP_CONFIG, AppConfig } from './config.token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpLoggerService implements Logger {
   _prefix= 'root';
-  constructor() { }
+  constructor(@Inject(APP_CONFIG) private appConfig:AppConfig) { 
+    console.log(this.appConfig)
+  }
   log(msg: string) {
     console.log(`${this._prefix} (Experimental): ${msg}`);
   }
